169ce8cfbade53ce5865edde866744bf3088e205
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2025-02-20 19:41:58 +0800

    update cors policy after correcting the wrong backend url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "*", // Explicitly define frontend origin
+  origin: "http://localhost:3000", // Explicitly define frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Allow credentials (cookies, sessions) 
   allowedHeaders: ["Content-Type", "Authorization"]
@@ -31,14 +31,14 @@ app.use(session({
   cookie: { secure: false, httpOnly: true, sameSite: 'Lax' } // Set to true if using HTTPS
 }));
 
-const allowedOrigins = "*"
+// const allowedOrigins = ["http://localhost:3000", "https://xxx"];
 
 app.use((req, res, next) => {  
   // const origin = req.headers.origin;
   // if (allowedOrigins.includes(origin)) {
   //   res.setHeader("Access-Control-Allow-Origin", origin);
   // }
-  res.header("Access-Control-Allow-Origin", allowedOrigins); // No wildcard when credentials: true
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // No wildcard when credentials: true
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
