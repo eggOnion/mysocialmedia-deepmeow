f7c55123c29c336fe3bdb9e163cd4b4bdefa31e0
Wei Cong TONG <137802716+eggOnion@users.noreply.github.com>
2025-02-21 10:10:39 +0800

    update cors settings

--- app.js
+++ app.js
@@ -32,24 +32,24 @@ app.use(session({
 }));
 
 
-// const allowedOrigins = ["http://localhost:3000", "https://mysocialmedia-deepmeow.vercel.app"];
-// app.use((req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
-//   // res.header("Access-Control-Allow-Origin", req.headers.origin); // No wildcard when credentials: true
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+const allowedOrigins = ["http://localhost:3000", "https://mysocialmedia-deepmeow.vercel.app"];
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  // res.header("Access-Control-Allow-Origin", req.headers.origin); // No wildcard when credentials: true
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
 
-//   // Handle preflight requests
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(204);
-//   }
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
 
-//   next();
-// });
+  next();
+});
 
 
 // Insert your routing HTML code here.
